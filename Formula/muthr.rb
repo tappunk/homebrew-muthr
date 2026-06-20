f24dbcceb5d8c495cd532d3db5019b381e39b816
tappunk <me@tappunk.com>
2026-06-21 00:35:28 +0900

    bump: muthr v0.1.26

--- Formula/muthr.rb
+++ Formula/muthr.rb
@@ -1,7 +1,7 @@
 class Muthr < Formula
   desc "Zero-trust orchestrator for secure inference and isolated AI agent execution"
   homepage "https://github.com/tappunk/muthr"
-  version "0.1.25"
+  version "0.1.26"
 
   depends_on arch: :arm64
   depends_on :macos
@@ -9,7 +9,7 @@ class Muthr < Formula
   depends_on "llama.cpp"
 
   url "https://github.com/tappunk/muthr/releases/download/v#{version}/muthr-#{version}-bin-macos-arm64.tar.gz"
-  sha256 "204285dfa263113222780b61a4593cbba054feaefc58cd540c970ddba9a8ce81"
+  sha256 "dc05c55dc06b47667926ad42c567c687b1c6b2c3b71be0204e8cbb2be39d5f7d"
 
   def install
     bin.install "muthr"
